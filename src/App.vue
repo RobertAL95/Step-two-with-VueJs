7b921bc788f28969ecb96428c20ec7ab66943882
RobertAL95 <robert.20.ven@gmail.com>
2021-08-06 22:24:36 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <Form />
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+export default {
+  name: "App",
+  components: {
+    Form: Form,
+  },
+};
+</script>
+
+<style lang="scss" >
+@import "./styles/_variables.scss";
+
+html,
+body {
+  background: $bg;
+  color: $color;
+  margin: 0;
+  font-family: sans-serif;
+}
+
+h2 {
+  margin-top: 0;
+  height: 50px;
+  background: $colorh2;
+  font-style: italic;
+  text-align: center;
+  //border-bottom: 1px solid $color-border;
+  paddin-bottom: 10px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,80 @@
+<template>
+  <h2>
+    Peliculas <br />
+    del fin de semana
+  </h2>
+
+  <form v-for="movie in movies">
+    <h3>{{ movie.name }}</h3>
+    <button
+      type="button"
+      v-on:click="movie.quantity -= 1"
+      :disabled="movie.quantity <= 0"
+    >
+      -
+    </button>
+    {{ movie.quantity }}
+    <button
+      type="button"
+      v-on:click="movie.quantity += 1"
+      :disabled="movie.quantity >= movie.available"
+    >
+      +
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  data() {
+    return {
+      movies: [
+        {
+          name: "Avengers",
+          available: 3,
+          quantity: 0,
+        },
+        {
+          name: "Terminator",
+          available: 5,
+          quantity: 0,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../styles/_variables.scss";
+form {
+  background: $colorh2;
+  //border: 1px solid $color;
+  border-radius: 10px;
+  margin: 0 50px 25px;
+  font-family: courier;
+  padding: 15px 25px 25px;
+  text-align: center;
+  transform: scale(1);
+  transition: 0.3s transform;
+}
+form:hover {
+  transform: scale(1.2);
+}
+button {
+  background: $color2;
+  border: 1px solid transparent;
+  border-radius: 5px;
+  cursor: pointer;
+  padding: 5px 10px;
+  transition: border-color 0.15;
+}
+button:hover {
+  border-color: $color-border;
+}
+button[disabled] {
+  opacity: 0.5;
+}
+</style>
